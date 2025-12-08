82b6187222f267b32a6ad726b897bad90feff67d
M TOQEER ZIA <muhammadtoqeerzia586694@gmail.com>
2025-12-08 11:58:05 +0500

    Refactor SellerProtectedRoute to include loading state and improve navigation logic

--- frontend/src/SelllerProtectedRoute.jsx
+++ frontend/src/SelllerProtectedRoute.jsx
@@ -1,13 +1,18 @@
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "../../backend/middlware/auth";
+import Loader from "./components/Layout/Loader"
 
-const SellerProtectedRoute = ({ isSeller,seller,children }) => {
-    if(!isSeller){
-        return <Navigate to={`/`} replace />;
+const SellerProtectedRoute = ({ children }) => {
+  const { isLoading, isSeller } = useSelector((state) => state.seller);
+
+  if (isLoading ===true) {
+    return (<Loader/>);
+  }else{
+    if (!isSeller) {
+      return <Navigate to="/shop-login" replace />;
     }
-  
     return children;
-};
-export default SellerProtectedRoute;
-
+  }
     
+  };
+export default SellerProtectedRoute;
