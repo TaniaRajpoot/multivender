8dc7d082b758d59191ee1933f3c085c94f38ab06
TaniaRajpoot <rajpoottania299@gmail.com>
2026-06-06 00:35:35 +0500

    fix: correct environment variable name for database connection

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(`${process.env.dbURL}`);
+    await mongoose.connect(`${process.env.DB_URL}`);
     console.log("Connected!");
   } catch (error) {
     console.error("Error", error);
